65d5d2fe935576911aa2b57dbe052eb9a170327e
Charles <charlesnwoye2@gmail.com>
2023-03-09 10:05:02 +0100

    get dispatches feature modified with JOIN statements

--- queries/dispatch.js
+++ queries/dispatch.js
@@ -1,4 +1,9 @@
-const getDispatchesQuery = `SELECT * FROM dispatch`;
+const getDispatchesQuery = `SELECT 
+                              DI.dispatch_id, DI.vehicle_id, V.model, 
+                              DR.driver_id, DR.first_name, DR.last_name, DR.phone
+                              FROM dispatch DI 
+                              INNER JOIN vehicle V ON DI.vehicle_id = V.vehicle_id
+                              INNER JOIN driver DR ON DI.driver_id = DR.driver_id`;
 
 const createDispatchQuery = `INSERT INTO
                               dispatch (dispatch_id, vehicle_id, driver_id)
