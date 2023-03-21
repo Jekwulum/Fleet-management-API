7d3622f76fd1ba80a97b4e4b7a934b44ac6ba498
Charles <charlesnwoye2@gmail.com>
2023-03-21 21:41:23 +0100

    added trips stats

--- controllers/stats.controllers.js
+++ controllers/stats.controllers.js
@@ -1,7 +1,7 @@
 const PoolConnector = require('../middlewares/services/connector.service');
 const { databaseError } = require('../middlewares/helpers/responses/database.responses');
-const { getActiveDriversCountQuery, getActiveVehiclesCountQuery,
-  getDriversCountQuery, getVehiclesCountQuery } = require('../queries/stats');
+const { getActiveDriversCountQuery, getActiveVehiclesCountQuery, getTripsCountQuery,
+  getDriversCountQuery, getVehiclesCountQuery, getTripsStatsQuery } = require('../queries/stats');
 
 const StatsController = {
   getStats: async (req, res) => {
@@ -10,11 +10,14 @@ const StatsController = {
         PoolConnector.query(getDriversCountQuery),
         PoolConnector.query(getActiveDriversCountQuery),
         PoolConnector.query(getVehiclesCountQuery),
-        PoolConnector.query(getActiveVehiclesCountQuery)
+        PoolConnector.query(getActiveVehiclesCountQuery),
+        PoolConnector.query(getTripsCountQuery),
+        PoolConnector.query(getTripsStatsQuery)
       ]).then(results => {
         const data = {
           drivers: { count: results[0]["rows"][0]["count"], active: results[1]["rows"][0]["count"] },
-          vehicles: { count: results[2]["rows"][0]["count"], active: results[3]["rows"][0]["count"] }
+          vehicles: { count: results[2]["rows"][0]["count"], active: results[3]["rows"][0]["count"] },
+          trips: { count: results[4]["rows"][0]["count"], stats: results[5]["rows"] }
         };
         res.status(200).json({ status: "SUCCESS", message: "succesfully fetched stats", data });
       })
--- queries/stats.js
+++ queries/stats.js
@@ -2,11 +2,15 @@ const getDriversCountQuery = "SELECT COUNT(driver_id) FROM driver";
 
 const getActiveDriversCountQuery = `SELECT COUNT(driver_id) FROM driver WHERE is_active = true`;
 
+const getTripsCountQuery = "SELECT COUNT(trip_id) FROM trips";
+
 const getVehiclesCountQuery = "SELECT COUNT(vehicle_id) FROM vehicle";
 
 const getActiveVehiclesCountQuery = `SELECT COUNT(vehicle_id) FROM vehicle WHERE is_active = true`;
 
+const getTripsStatsQuery = `SELECT SUM(distance) as distance, COUNT(trip_date), trip_date AS date FROM trips GROUP BY trip_date`;
+
 module.exports = {
   getDriversCountQuery, getActiveDriversCountQuery, getActiveVehiclesCountQuery,
-  getVehiclesCountQuery
+  getVehiclesCountQuery, getTripsCountQuery, getTripsStatsQuery
 };
